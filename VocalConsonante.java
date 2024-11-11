8e55f180218da8eab0b3fa4695537dd29275aa2f
Francisco8669 <24031135@itcelaya.edu.mx>
2024-11-11 09:07:12 -0600

    Segundo commit

--- VocalConsonante.java
+++ VocalConsonante.java
@@ -1,10 +1,31 @@
+
+package vocalconsonante;
+
+import java.util.Scanner;
+
+
 public class VocalConsonante {
     public static void main(String[] args) {
-        char ch = args[0].charAt(0);
-
-        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
-            System.out.println(ch + " es una vocal.");
-        else 
-            System.out.println(ch + " es una consonante.");
+        //char ch = args[0].charAt(0);
+        char car;
+        String aux;
+        Scanner sc = new Scanner(System.in);
+        do  {
+            aux = sc.next().toLowerCase();
+            car = aux.charAt(0);
+            if (car != 'q') {
+                if (car == 'a' || car == 'e' || car == 'i' || car == 'o' || car == 'u'){
+                    System.out.println("Es vocal");
+                } 
+                else {
+                    if(car == '1' || car == '2' || car == '3' || car == '4' || car == '5' || car == '6' || car == '7' || car == '8' || car == '9' || car == '0' ){
+                        System.out.println("No es vocal, ni consonante");
+                    }
+                    else{
+                        System.out.println("Es consonante");
+                    }
+                }
+            }
+        } while  (car != 'q');
     }
 }
